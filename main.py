e8e1ad708fd4be35ba1c9c572f51a26a7eb6aaa0
Katherine Qiao <17706734+kgqiao@users.noreply.github.com>
2024-10-22 21:52:59 -0700

    2nd push

--- main.py
+++ main.py
@@ -1,5 +1,99 @@
 
+'''
+#Project: Image Segmentation
+1. HOWTO: Ugrade Pip Install in Python Terminal
+PS H:\CODING\PROJECT_IST\ImageSegmentation> python.exe -m pip install --upgrade pip      
+
+'''
+
+#USE TERMINAL: Powershell
+
+
+#################### Define Libraries #####################################
 
 #1. Install required Python packages libraries
-#Write in terminal: pip instlal nump
+#Write in terminal: pip install numpy
+#linear algebra
 import numpy as np
+
+#Write in Terminal: pip install cv
+import cv
+
+#Write in terminal: matplotlib
+from matplotlib import pyplot as plt
+
+#Import CSV library
+import csv
+
+
+##################### DEFINE CONSTANTS #####################################
+
+
+
+
+
+
+
+
+
+##################### GLOBAL VARIABLES ######################################
+
+
+
+
+
+
+
+
+################### PSEUDOCODE #############################################
+
+'''
+1. Look for K number of segments in the image
+2. Take an image to a feature space
+3. Make K random initial means
+4. Create K clusters by assigning for each point to the nearest initial mean
+5. Re-compute the cluster’s means
+6. If changes in all K means is less than a certain threshold e, stop. Else, re-compute clusters’ means until this condition is met. 
+
+'''
+
+#################### MAIN CODE ##############################################
+
+
+def main():
+
+
+
+
+
+
+
+
+def identify_segments():
+    #Identify K number of segments
+
+
+
+
+
+
+
+def
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
